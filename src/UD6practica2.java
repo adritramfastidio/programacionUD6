cacd51241b325452f37c0af42a0166c679051b63
adritramfastidio <adritramfastidio@gmail.com>
2025-11-12 11:27:40 +0100

    empezando ej 2 practica 2

--- src/UD6practica2.java
+++ src/UD6practica2.java
@@ -7,29 +7,72 @@ public class UD6practica2 {
     public static void main(String[] args) {
         int opc = 0;
         do {
-            Utils.menuEjercicios(16);
+            Utils.menuEjercicios(8);
             opc = Utils.validaInt();
             //sc.nextLine();
             switch (opc) {
                 case 1 -> ejercicio1();
-                case 17 -> System.out.println("Hasta la próxima!");
+                case 2 -> ejercicio2();
+                case 9 -> System.out.println("Hasta la próxima!");
                 default -> System.out.println("Opción no válida.");
             }
-        } while (opc != 17);
+        } while (opc != 9);
     }
 
     /*1. Escribe un programa que pida dos números reales por teclado y muestre por
     pantalla el resultado de multiplicarlos. */
-    public static void ejercicio1() {
-        System.out.print("Dime un número: ");
-        double n1 = Utils.validaDouble();
-        System.out.print("Dime otro número: ");
-        double n2 = Utils.validaDouble();
+        public static void ejercicio1() {
+            int num;
+            System.out.println("Introduce un número para saber si es primo. Introduce 0 para salir.");
+            do{
+                System.out.print("Dime un número: ");
+                num = Utils.validaInt();
+                Utils.validaPositivoInt(num);
+                if(esPrimo(num)){
+                    System.out.println(num + " es primo.");
+                } else {
+                    System.out.println(num + " no es primo.");
+                }
+            } while (num != 0);
+        }
+        public static boolean esPrimo(int num) {
+            if (num == 1) {
+                return false;
+            } else if (num == 2) {
+                return true;
+            } else {
+                for (int i = 2; i * i <= num; i++) {
+                    if (num % i == 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
 
-        System.out.println("Su multiplicación es: " + multiplica(n1, n2));
-    }
-
-    public static double multiplica(double n1, double n2) {
-        return n1 * n2;
-    }
+    /*Escribe un programa que pida el DNI y muestre por pantalla la letra asociada.
+    Para ello se deberá crear una función a la que se le pase el número y devuelva la
+    letra. */
+        public static void ejercicio2() {
+            int num;
+            System.out.println(56321122 % 23);
+            do{
+                System.out.print("Dime un número: ");
+                num = Utils.validaInt();
+                Utils.validaPositivoInt(num);
+                if(esPrimo(num)){
+                    System.out.println(num + " es primo.");
+                } else {
+                    System.out.println(num + " no es primo.");
+                }
+            } while (num != 0);
+        }
+        public static void letraDNI(int num) {
+            switch (num % 23) {
+                case 1 -> ejercicio1();
+                case 2 -> ejercicio2();
+                case 9 -> System.out.println("Hasta la próxima!");
+                default -> System.out.println("Opción no válida.");
+            }
+        }
 }
